3523875e342299a0a81db28f32a8961f3ccdb8c3
Daniel Zaslavsky <dani.zaslavsky@gmail.com>
2023-06-20 16:16:29 +0300

    NAVBAR WORKS

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <!-- <div id="app">
     <div id="nav">
       <router-link :to="{ name: 'main' }">Vue Recipes</router-link>|
       <router-link :to="{ name: 'search' }">Search</router-link>|
@@ -14,7 +14,45 @@
       </span>
     </div>
     <router-view />
-  </div>
+  </div> -->
+  <div id = "app">
+    <b-navbar toggleable="lg" type="dark" variant="info">
+      <b-navbar-brand href="#">RecipeWebsite</b-navbar-brand>
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item :to="{ name: 'main' }">HOME</b-nav-item>
+          <b-nav-item :to="{ name: 'search' }">Search</b-nav-item>
+          <b-nav-item>
+            <span v-if="!$root.store.username">
+              Guest:
+              <b-nav-item :to="{ name: 'register' }">Register</b-nav-item>|
+              <b-nav-item :to="{ name: 'login' }">Login</b-nav-item>
+            </span>
+            <span v-else>
+              {{ $root.store.username }}: <button @click="Logout">Logout</button>
+            </span>
+          </b-nav-item>
+        </b-navbar-nav>
+
+        <b-navbar-nav class="ml-auto">
+          <b-nav-form>
+            <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
+            <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+          </b-nav-form>
+
+          <b-nav-item-dropdown right>
+            <template #button-content>
+              <em>User</em>
+            </template>
+            <b-dropdown-item href="#">Profile</b-dropdown-item>
+            <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+          </b-nav-item-dropdown>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+    <router-view />
+    </div>
 </template>
 
 <script>
