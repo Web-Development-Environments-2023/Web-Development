9abb774b37661364b4231e06c0c5b7a776459e7d
Daniel Zaslavsky <dani.zaslavsky@gmail.com>
2023-06-29 15:13:40 +0300

    fav to recipePrev

--- src/components/RecipePreview.vue
+++ src/components/RecipePreview.vue
@@ -1,108 +1,149 @@
 <template>
-    <div class="RecipePreview">
-        <a href="#" class="recipe-preview" @click="navigateToRecipe">
-            <b-card :img-src="recipe.image" img-alt="Image" img-top tag="article" class="mb-2">
-                <div class="recipe-details">
-                    <h3 class="recipe-title">{{ recipe.title }}</h3>
-                    <p>Ready in {{ recipe.readyInMinutes }} minutes.</p>
-                    <p>Popularity: {{ recipe.aggregateLikes }}.</p>
-                    <p>Vegan: {{ recipe.vegan ? 'Yes' : 'No' }}.</p>
-                    <p>Gluten-free: {{ recipe.glutenFree ? 'Yes' : 'No' }}.</p>
-                </div>
-            </b-card>
-        </a>
-    </div>
+  <div class="RecipePreview">
+    <a href="#" class="recipe-preview" @click="navigateToRecipe">
+      <b-card :img-src="recipe.image" img-alt="Image" img-top tag="article" class="mb-2">
+        <div class="recipe-details">
+          <h3 class="recipe-title">{{ recipe.title }}</h3>
+          <div class="fav-to-the-right">
+            <button v-if="!$root.store.username" class="fav-no-logged-in">Log in to add to favs</button>
+            <button v-else-if="!isFavorite" class="add-to-favorite" @click="addToFavorites">Add to favorite</button>
+            <button v-else class="in-favorites">In favorites</button>
+          </div>
+          <p>Ready in {{ recipe.readyInMinutes }} minutes.</p>
+          <p>Popularity: {{ recipe.aggregateLikes }}.</p>
+          <p>Vegan: {{ recipe.vegan ? 'Yes' : 'No' }}.</p>
+          <p>Gluten-free: {{ recipe.glutenFree ? 'Yes' : 'No' }}.</p>
+        </div>
+      </b-card>
+    </a>
+  </div>
 </template>
 
 <script>
 import { state } from "/src/store.js";
 
 export default {
-    name: "RecipePreview",
-    props: {
-        recipe: {
-            type: Object,
-            required: true
-        }
-    },
-    methods: {
-        navigateToRecipe() {
-            const payload = {
-                recipeId: this.recipe.id
-            };
-            this.axios
-                .post(`${state.server_domain}/users/watched`, payload, {
-                    headers: {
-                        'Content-Type': 'application/json'
-                    }
-                })
-                .then(response => { /* console.log('Recipe ID logged successfully:', response.data); */ })
-                .catch(error => { console.error('Error logging recipe ID:', error); })
-                .finally(() => { this.$router.push({ name: 'recipe', params: { recipeId: payload.recipeId } }); });
-        }
+  name: "RecipePreview",
+  props: {
+    recipe: {
+      type: Object,
+      required: true
     }
+  },
+  methods: {
+    navigateToRecipe(event) {
+      if (event.target.tagName !== 'BUTTON') {
+        const payload = {
+          recipeId: this.recipe.id
+        };
+        this.axios
+          .post(`${state.server_domain}/users/watched`, payload, {
+            headers: {
+              'Content-Type': 'application/json'
+            }
+          })
+          .then(response => { /* console.log('Recipe ID logged successfully:', response.data); */ })
+          .catch(error => { console.error('Error logging recipe ID:', error); })
+          .finally(() => { this.$router.push({ name: 'recipe', params: { recipeId: payload.recipeId } }); });
+      }
+    },
+    addToFavorites() {
+      const payload = {
+        recipeId: this.recipe.id
+      };
+      this.axios
+        .post(`${state.server_domain}/users/favorites`, payload, {
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        })
+        .then(response => { /* console.log('Recipe ID logged successfully:', response.data); */ })
+        .catch(error => { console.error('Error logging recipe ID:', error); })
+    }
+  }
 };
 </script>
 
 <style scoped>
 .RecipePreview {
-    margin-bottom: 20px;
+  margin-bottom: 20px;
 }
 
 .RecipePreview .recipe-preview {
-    text-decoration: none;
+  text-decoration: none;
 }
 
 .RecipePreview .recipe-preview:hover {
-    opacity: 0.8;
+  opacity: 0.8;
 }
 
 .RecipePreview .recipe-card {
-    border: none;
-    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  border: none;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
 }
 
 .RecipePreview .recipe-card img {
-    width: 100%;
-    height: auto;
+  width: 100%;
+  height: auto;
 }
 
 .RecipePreview .recipe-title {
-    font-size: 18px;
-    font-weight: bold;
-    margin-top: 10px;
+  font-size: 18px;
+  font-weight: bold;
+  margin-top: 10px;
 }
 
 .RecipePreview .recipe-details {
-    padding: 10px;
+  padding: 10px;
 }
 
 .RecipePreview .recipe-details p {
-    margin-bottom: 5px;
-    font-size: 14px;
-    color: #888;
+  margin-bottom: 5px;
+  font-size: 14px;
+  color: #888;
 }
 
 .RecipePreview .recipe-details .popularity {
-    color: #f95c5c;
+  color: #f95c5c;
 }
 
 .RecipePreview .recipe-details .vegan,
 .RecipePreview .recipe-details .gluten-free {
-    color: #6cc070;
+  color: #6cc070;
 }
 
 .RecipePreview .recipe-details .vegan::before,
 .RecipePreview .recipe-details .gluten-free::before {
-    content: "•";
-    margin-right: 5px;
+  content: "•";
+  margin-right: 5px;
 }
 
 .RecipePreview .recipe-details .vegan::before {
-    color: #6cc070;
+  color: #6cc070;
 }
 
 .RecipePreview .recipe-details .gluten-free::before {
-    color: #6cc070;
+  color: #6cc070;
+}
+
+.add-to-favorite {
+  color: red;
+}
+
+.fav-no-logged-in {
+  color: red;
+  background-color: lightgray;
+  cursor: not-allowed;
+}
+
+.in-favorites {
+  color: red;
+  background-color: lightgray;
+  cursor: not-allowed;
+}
+
+.fav-to-the-right {
+  display: flex;
+  justify-content: flex-end;
 }
 </style>
