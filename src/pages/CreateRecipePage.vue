d25656771f9555c41e7a8e9eba36859690b00c80
NirKors <103315566+NirKors@users.noreply.github.com>
2023-07-11 13:46:01 +0300

    CreateRecipePage

--- src/pages/CreateRecipePage.vue
+++ src/pages/CreateRecipePage.vue
@@ -1,6 +1,6 @@
 <template>
-    <b-modal v-model="modalVisible" title="Create Recipe" @hidden="$emit('close')">
-        <form @submit="submitForm">
+    <b-modal v-model="modalVisible" title="Create Recipe" @hidden="$emit('close')" hide-footer>
+        <form @submit.prevent="submitForm">
             <b-form-group label="Image URL:" label-for="image-input">
                 <b-form-input id="image-input" v-model="recipe.image" required></b-form-input>
             </b-form-group>
@@ -26,16 +26,16 @@
                 <b-form-input id="numOfServings-input" type="number" v-model="recipe.numOfServings" required></b-form-input>
             </b-form-group>
 
-            <b-button type="submit">Create Recipe</b-button>
+            <b-button type="submit" variant="primary">Create Recipe</b-button>
         </form>
         <div v-if="errorMsg" class="alert">
             <h2>Error!</h2>
-            <p>{{ errorMessage }}</p>
+            <p>{{ errorMsg }}</p>
             <button @click="resetError">Close</button>
         </div>
     </b-modal>
 </template>
-
+  
 <script>
 import { state } from "/src/store.js";
 
@@ -54,13 +54,20 @@ export default {
                 preparationSteps: "",
                 numOfServings: null,
             },
-            errorMsg: null
+            errorMsg: null,
         };
     },
     methods: {
-        submitForm(){
-            this.createRecipe();
-            this,this.modalVisible = false;
+        async submitForm() {
+            try {
+                const success = await this.createRecipe();
+                if (success) {
+                    this.modalVisible = false; // Close the modal on success
+                }
+            } catch (error) {
+                console.error('Error logging recipe ID:', error);
+                this.errorMsg = error.response.data.message + ".";
+            }
         },
         createRecipe() {
             const payload = {
@@ -74,7 +81,7 @@ export default {
                 num_of_servings: this.recipe.numOfServings, // Updated property name
             };
 
-            this.axios
+            return this.axios
                 .post(`${state.server_domain}/users/recipe_creation`, payload, {
                     headers: {
                         'Content-Type': 'application/json',
@@ -82,19 +89,63 @@ export default {
                 })
                 .then(response => {
                     console.log('Recipe ID logged successfully:', response.data);
+                    return true;
                 })
                 .catch(error => {
-                    console.error('Error logging recipe ID:', error);
-                    this.errorMsg = error.message;
-                })
+                    throw error;
+                });
         },
         resetError() {
             this.errorMsg = null; // Reset the error message
-        }
+        },
     },
 };
 </script>
+  
 
 <style scoped>
-/* placeholder */
+.modal-content {
+    padding: 20px;
+}
+
+.form-group {
+    margin-bottom: 20px;
+}
+
+.checkbox-label {
+    margin-right: 10px;
+}
+
+textarea {
+    width: 100%;
+    height: 150px;
+    resize: vertical;
+    margin-bottom: 10px;
+}
+
+.alert {
+    margin-top: 20px;
+    padding: 10px;
+    background-color: #f8d7da;
+    color: #721c24;
+}
+
+.alert h2 {
+    margin-top: 0;
+    margin-bottom: 5px;
+    font-size: 18px;
+}
+
+.alert p {
+    margin: 0;
+}
+
+.alert button {
+    margin-top: 10px;
+    padding: 5px 10px;
+    background-color: #dc3545;
+    color: #fff;
+    border: none;
+    cursor: pointer;
+}
 </style>
