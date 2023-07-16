96e727991f3a73df1fbbe7a39d451f4d92f9d937
NirKors <103315566+NirKors@users.noreply.github.com>
2023-07-16 16:34:51 +0300

    Swap to non-local

--- src/store.js
+++ src/store.js
@@ -1,5 +1,5 @@
 export const state = {
     // server_domain: ""
-    server_domain: "http://localhost:3000",
-    // server_domain: "http://132.72.65.211/",
+    // server_domain: "http://localhost:3000",
+    server_domain: "http://132.73.84.127/",
 };
--- vue.config.js
+++ vue.config.js
@@ -3,8 +3,8 @@ module.exports = {
     devtool: "source-map"
   },
   devServer: {
-    // host: "132.72.65.211"
-    host: "localhost"
+    // host: "localhost"
+    host: "132.73.84.127"
   }
   // publicPath: process.env.NODE_ENV === "production" ? "/LAB12/" : "/"
 };
